cf4821914ad8e028c785bb3562fae8ed475890b9
schaefchen40 <schaefchen@github.com>
2018-03-25 14:08:31 +0200

    distance calculation

--- map.js
+++ map.js
@@ -1,5 +1,5 @@
-var mymap = L.map('mapid').setView([47.583807, 12.1736679], 18);
-//var map = L.map('map').setView([45.8167, 15.9833], 10);
+// var mymap = L.map('mapid').setView([47.583807, 12.1736679], 9);      //location of fh kufstein
+var mymap = L.map('mapid').setView([47.583807, 11.9], 9);         //location of innsbruck (for testing)
 
 var LeafIcon = L.Icon.extend({
     options: {
@@ -19,11 +19,17 @@ var doneIcon = new LeafIcon({
 
 L.tileLayer('https://maps4.wien.gv.at/basemap/bmaporthofoto30cm/normal/google3857/{z}/{y}/{x}.jpeg' , {
 }).addTo( mymap );
-var marker = L.marker([47.583807, 12.1736679], {icon: openIcon}).bindPopup("Kaffee?").addTo(mymap);
-var marker2 = L.marker([47.583707, 12.173679], {icon: openIcon}).bindPopup("Kaffee?").addTo(mymap);
-var marker3 = L.marker([47.583707, 12.173579], {icon: openIcon}).bindPopup("Kaffee?").addTo(mymap);
-var marker4 = L.marker([47.583807, 12.173579], {icon: openIcon}).bindPopup("Kaffee?").addTo(mymap);
 
+var destMarker = [
+    [47.583857, 12.173269],
+    [47.583857, 12.173679],
+    [47.583707, 12.173279],
+    [47.583707, 12.173679]];                   //destination Markers
+var marker = L.marker(destMarker[0], {icon: openIcon}).bindPopup("Kaffee?").addTo(mymap);
+var marker2 = L.marker(destMarker[1], {icon: openIcon}).bindPopup("Kaffee?").addTo(mymap);
+var marker3 = L.marker(destMarker[2], {icon: openIcon}).bindPopup("Kaffee?").addTo(mymap);
+var marker4 = L.marker(destMarker[3], {icon: openIcon}).bindPopup("Kaffee?").addTo(mymap);
+var locationMarker;
 mymap.removeControl(mymap.zoomControl);
 
 if (!navigator.geolocation){
@@ -31,17 +37,25 @@ if (!navigator.geolocation){
   }else{
     function success(position) {
         console.log(position.coords.latitude);
-        L.marker([position.coords.latitude,position.coords.longitude]).addTo(mymap);
+        var newPosition = L.latLng(position.coords.latitude,position.coords.longitude);
+        if(locationMarker === undefined){
+            locationMarker = new L.marker(newPosition).addTo(mymap);
+            console.log(locationMarker.getLatLng());
+            console.log((locationMarker.getLatLng().distanceTo(marker.getLatLng())));
+        }else{
+            locationMarker.setLatLng(newPosition);
+        }
     }
     
     function error() {
-        alert("Sorry, no position available.");
+        // alert("Sorry, no position available.");
+        console.log("watchPosition: error");
     }
     
     var geo_options = {
         enableHighAccuracy: false, 
-        maximumAge        : 5000, 
-        timeout           : 4000
+        maximumAge        : 6000, 
+        timeout           : 5000
     };
     
     navigator.geolocation.watchPosition(success, error, geo_options);
