75105f8bbbb15fd4988fd350e7f44019bf47138d
u8rg7af <hel-electronics@gmx.at>
2018-06-05 11:20:32 +0200

    Add files via upload

---
+++ client.js
@@ -0,0 +1,62 @@
+var W3CWebSocket = require('websocket').w3cwebsocket;
+
+var msg = {
+    latitude: "0",
+    longitude: "0",
+    type: "message",
+    text: "Walder Helmut",
+    id:   "55",
+    date: Date.now()
+};
+
+
+var client = new W3CWebSocket('ws://80.122.77.19:5003/', 'echo-protocol');
+ 
+client.onerror = function() {
+    console.log('Connection Error');
+};
+ 
+client.onopen = function() {
+    console.log('WebSocket Client Connected');
+ 
+    
+    function sendPosition(position) {
+        msg.latitude = position.coords.latitude;
+ 
+        msg.longitude = position.coords.longitude;
+
+        myLocation = {lat: latitude, lng: longitude};
+        //marker = {lat: latitude, lng: longitude};
+
+        //sock.send(JSON.stringify(msg));
+    }
+    
+    
+    
+    function sendLocation() {
+        if (client.readyState === client.OPEN) {
+            client.send(JSON.stringify(msg));
+
+            setTimeout(sendLocation, 2000);
+        }
+    }
+    sendLocation();
+
+    function getLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(sendPosition);
+            
+        } 
+    }
+
+};
+ 
+client.onclose = function() {
+    console.log('Connection closed');
+};
+ 
+client.onmessage = function(e) {
+    if (typeof e.data === 'string') {
+        console.log("Received: '" + e.data + "'");
+    }
+};
